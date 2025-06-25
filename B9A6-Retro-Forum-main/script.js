a82095750c98008dbc7ce7e387275c144172926e
Shuvo <nhshuvo2003@gmail.com>
2025-06-25 09:00:58 +0600

    first commit

---
+++ B9A6-Retro-Forum-main/script.js
@@ -0,0 +1,10 @@
+const hoverButton =()=>{
+    const buttions=document.querySelectorAll('navBtn');
+    buttions.forEach(buttion=>
+    {
+        buttion.addEventListener('mouseover'()=>{
+            console.log(ok)
+        })
+    }
+    )
+}
